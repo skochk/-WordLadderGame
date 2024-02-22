d47c3a56791ad93cf16c0b98fb50e78c0ce4e852
skochk <kochkares@gmail.com>
2024-02-22 18:10:19 +0100

    playboard word selection and check

--- frontend/my-app/src/classes/gameClass.tsx
+++ frontend/my-app/src/classes/gameClass.tsx
@@ -1,5 +1,19 @@
+// class Playboard{
+//     public gameGrid: number[][];
+//     // public wordList: string[];
+
+//     public constructor(gameGrid: number[][]){
+//         this.gameGrid = gameGrid;
+//     }
+
+//     public getGameGrid(): number[][]{
+//         return this.gameGrid;
+//     }
+
+// }
+// // const plb = new Playboard([])
+// // plb.gameGrid
 
 
 
-
-export {}
+// export {}
